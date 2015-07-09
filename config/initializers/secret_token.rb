786c51ad7880c81ebdc6025c1b2419d2370c88e4
woodyeire <johnmurphyty@gmail.com>
2015-07-09 10:44:54 +0000

    Created a rails app and added Status Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clonyn::Application.config.secret_token = '8c188bae820f716093ed4661667e6aa03c6a82a51fd43d37b37de2df9d2657df055a2e97c6462222aac6b178f951771b204812615dbbf3e9bcbc01b2936c36dd'
