c2b7b203ca35851b512038c87212bce41ad0e1d2
woodyeire <johnmurphyty@gmail.com>
2015-09-13 19:32:13 +0000

    blah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,13 +17,13 @@ Clonyn::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
   
   #custom
-  config.assets.precompile += %w( user_friendships.js application.js bootstrap.js application.css bootstrap.css statuses.css.scss  )
+  # config.assets.precompile += %w( user_friendships.js application.js bootstrap.js application.css bootstrap.css statuses.css.scss  )
   
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
