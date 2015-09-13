f72c2b4279a838288a013415eabcd328c4bac037
woodyeire <johnmurphyty@gmail.com>
2015-09-13 21:19:36 +0000

    disabled mail notification

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -15,7 +15,7 @@ class UserFriendship < ActiveRecord::Base
     after_destroy :delete_mutual_friendship!
     
     state_machine :state, initial: :pending do
-        after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship]
+        after_transition on: :accept, do: [:accept_mutual_friendship]
         after_transition on: :block, do: [:block_mutual_friendship]
         
         state :requested
